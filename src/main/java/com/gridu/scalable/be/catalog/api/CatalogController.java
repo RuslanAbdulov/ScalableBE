fcef11f0f8e6c1f386a2590df80368357d381ba0
rabdulov <ratioff@list.ru>
2019-06-25 23:38:53 +0400

    catalog api path

--- src/main/java/com/gridu/scalable/be/catalog/api/CatalogController.java
+++ src/main/java/com/gridu/scalable/be/catalog/api/CatalogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/catalog/products")
 @SuppressWarnings("unchecked")
 public class CatalogController {
 
